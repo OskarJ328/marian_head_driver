d656a99c61221d51455ff579434cee25e92ef627
Ozkerror <ozkerror@gmail.com>
2025-12-03 21:09:10 +0100

    konfiguracja rejestrow uarta

--- stm32_driver/Src/uart.c
+++ stm32_driver/Src/uart.c
@@ -5,4 +5,40 @@
  *      Author: ozker
  */
 
+ #include "uart.h"
+
+ /*
+ wyznaczanie wartosci rejestru BRR:
+ USARTDIV = USART_CLK / (OVERSAMPLING * BAUDRATE)
+ OVERSAMPLING - ZALEZY OD WARTOSCI BITU OVER8 W REJESTRZE CR1
+ BAUDRATE - predkosc jaka chcemy aby miala nasza transmisja
+ MANTISSA - calkowita czesc wyniku usartdiv
+ FRACTION - czesc po przecinku, w celu wpisania jej do rejestru nalezy przemnozyc ją
+ przez oversampling i zaokraglic do liczby calkowitej.
+ */
+
+#define MANTISSA 8
+#define FRACTION 11
+
+#define UART2_SET_BRR USART2->BRR = MANTISSA << 4 | FRACTION
+#define UART2_RE USART2->CR1 |= 1 << 2
+#define UART2_TE USART2->CR1 |= 1 << 3
+#define UART2_EN USART2->CR1 |= 1 << 13
+
+void uart_init(void){
+    uart2_init();
+}
+
+
+void uart2_init(void){
+    UART2_SET_BRR;
+    UART2_TE;
+    UART2_RE;
+    UART2_EN;
+}
+ 
+
+
+
+
 
